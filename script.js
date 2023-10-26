ba3dd3705611a93115b6ba78a06134ad4db1e451
3imaggie2 <80319729+3imaggie2@users.noreply.github.com>
2023-10-26 18:41:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open')
+};
+
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal('.container h6',{delay:150, origin: 'left'});
+
+sr.reveal('.container h3',{delay:250, origin: 'right'});
+
+sr.reveal('.container h1',{delay:400, origin: 'top'});
+
+sr.reveal('.container p',{delay:550, origin: 'left'});
+
+sr.reveal('.main-btnn',{delay:700, origin: 'bottom'});
+
+sr.reveal('.social', {delay:850, origin: 'right'});
